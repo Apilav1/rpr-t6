fc6b4c04ade8f013b6ac4e309d989b0a9e6f789c
Apilav1 <apilav1@etf.unsa.ba>
2018-11-28 22:09:29 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,29 @@
+package sample;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.InputMethodEvent;
+
+import javax.swing.*;
+
+public class Controller {
+    public TextField ime;
+    public TextField prezime;
+    public TextField broj_indexa;
+    public TextField JMBG;
+    public TextField mjesto_rodjenja;
+    public TextField adresa;
+    public TextField telefon;
+    public TextField email;
+    public DatePicker datum_rodjenja;
+
+
+}
